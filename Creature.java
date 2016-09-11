3e7cb1530ef4d35b7b524e119f5caeb35dbe0822
detroitcoder9 <alexmazur6@gmail.com>
2016-09-11 18:48:47 -0400

    Added getDamage functionality

--- Creature.java
+++ Creature.java
@@ -32,13 +32,35 @@ public class Creature {
    }
    
    public int getDamage() {
-      //placeholder
+      int damage = 1 + (int)(Math.random() * hitPoints);
+      if (species.equals("Balrog") || species.equals("Cyberdemon")) {
+         double critChance = Math.random();
+         if (critChance < 0.05) {
+            damage = damage + 50;
+         }
+      }
+      else if (species.equals("Elves")) {
+         double chance = Math.random();
+         if (chance < 0.1) {
+            return = damage + damage;
+         }
+      }
+      
+      else if (species.equals("Balrog")) {
+         int damage2 = 1 + (int)(Math.random() * hitPoints);
+         return = damage + damage2;
+      }
+      else {
+         return damage;
+      }         
    }
    
    public void Damage(int damage) throws exception {
+      //TODO
    }
    
    public boolean isDead() {
+      //TODO
    }
    
    public String getName() {
